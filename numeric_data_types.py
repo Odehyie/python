a935a336907edf9e9ec905d74245e69c3bccb083
Darlington Djaba <darlingtondjaba@gmail.com>
2024-05-07 23:41:54 +0000

    Update numeric_data_types.py

--- numeric_data_types.py
+++ numeric_data_types.py
@@ -3,12 +3,28 @@ print("Python has three numeric types: int, float, and complex")
 #creating a variable
 myValue=1
 print(myValue)
-
 print(type(myValue))
-
 print(str(myValue) + " is of the data type " + str(type(myValue)))
 
+#float data types
 myValue=3.14
 print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
 
+#complex data type
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
 
+#bool data type
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
