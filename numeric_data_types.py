a03ccb85164ee2f2621905a386f4101b4c1e7bcd
Darlington Djaba <darlingtondjaba@gmail.com>
2024-05-07 23:29:30 +0000

    Create numeric_data_types.py

---
+++ numeric_data_types.py
@@ -0,0 +1,14 @@
+print("Python has three numeric types: int, float, and complex")
+
+#creating a variable
+myValue=1
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=3.14
+print(myValue)
+
+
